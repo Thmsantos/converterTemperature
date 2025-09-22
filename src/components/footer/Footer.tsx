741f8558f458566252174d84f7632b6ffc0d353d
Marigsiqueira <mgsmariana123@gmail.com>
2025-09-22 00:34:12 -0300

    :sparkling_heart: Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,20 @@
 import './Footer.css'
+import fatecLogo from '../../../public/fatec.png';
 
-export default function Footer(){
-    return(
-        <p className='text'>
-            oi
-        </p>
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <a  href="https://fatecdiadema.cps.sp.gov.br/" className="footer-text">
+                <img className='footerImg' src={fatecLogo}/>
+            </a>
+            <div className="footer-links">
+                <a href="https://github.com/Thmsantos/converterTemperature" target="_blank" rel="noreferrer">
+                    GitHub
+                </a>
+                <a href="https://www.instagram.com/fatec.diadema/" target="_blank" rel="noreferrer">
+                    Instagram
+                </a>
+            </div>
+        </footer>
     )
 }
